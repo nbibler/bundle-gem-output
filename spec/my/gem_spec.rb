0f1d488a1f1d9e37493a029cb93a739b395f082b
Nathaniel Bibler <git@nathanielbibler.com>
2017-05-22 22:10:45 -0400

    1.13.0.rc.2

--- spec/my/gem_spec.rb
+++ spec/my/gem_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe My::Gem do
-  it 'has a version number' do
+  it "has a version number" do
     expect(My::Gem::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  it "does something useful" do
     expect(false).to eq(true)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'my/gem'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "my/gem"
